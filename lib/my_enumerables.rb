172cee21da60d3cd32c9917a4b938e175bf25243
aishyuu <ivanmatias1331@gmail.com>
2024-04-06 15:21:33 -0700

    Finished problems

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,55 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    self.each_with_index do |elem, index|
+      yield(elem, index)
+    end
+    self
+  end
+
+  def my_select
+    result = []
+    self.each do |elem|
+      result.push(elem) if yield(elem)
+    end
+    result
+  end
+
+  def my_all?
+    result = []
+    self.each do |elem|
+      result.push(elem) if yield(elem)
+    end
+    return result.length == self.length
+  end
+
+  def my_any?
+    result = []
+    self.each do |elem|
+      result.push(elem) if yield(elem)
+    end
+    return result.length >= 1
+  end
+
+  def my_none?
+    result = []
+    self.each do |elem|
+      result.push(elem) if yield(elem)
+    end
+    return result.length == 0
+  end
+
+  def my_count
+    if !block_given?
+      return self.length
+    end
+    result = 0
+    self.each do |elem|
+      if yield(elem)
+          result += 1
+      end
+    end
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +57,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    self.each do |elem|
+      yield(elem)
+    end
+    self
+  end
 end
